0f6ed321877f06d0b9e9b8a2afff4eebc4d50f41
BrotchMrToast <120139392+BrotchMrToast@users.noreply.github.com>
2025-06-03 19:57:00 +0200

    Create Header.jsx

---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+@tailwind base;
+@tailwind components;
+@tailwind utilities;
+
+body {
+  @apply bg-blue-50 text-gray-800 font-sans;
+}
+
+html, body, #root {
+  height: 100%;
+  margin: 0;
+  padding: 0;
+}
