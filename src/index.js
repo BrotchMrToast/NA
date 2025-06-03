b970ec6e346315afe776b7cf9d9aa823650656bd
BrotchMrToast <120139392+BrotchMrToast@users.noreply.github.com>
2025-06-03 19:09:16 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
+// File: src/index.js
+// Purpose: Entry point for React app
 
 import React from 'react';
 import ReactDOM from 'react-dom/client';
@@ -5,4 +7,8 @@ import './index.css';
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<React.StrictMode><App /></React.StrictMode>);
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
