a0c7cb97a8b153ccbc5846e297fe38524360523a
BrotchMrToast <120139392+BrotchMrToast@users.noreply.github.com>
2025-06-09 21:39:53 +0200

    Update supabaseClient.js

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseUrl = import.meta.env.postgresql://postgres:[YOUR-PASSWORD]@db.flgdzbbrwqjqprdxhgqf.supabase.co:5432/postgres;
 const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
